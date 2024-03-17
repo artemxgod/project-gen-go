c309b8ff5e2a4cb14bd0f1c7ba19f625af21b275
Artem Kain <artemgod@Professional.local>
2024-03-17 15:29:45 +0300

    add docker

--- internal/generator/default.go
+++ internal/generator/default.go
@@ -3,6 +3,7 @@ package generator
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 )
@@ -27,6 +28,9 @@ func (ag ApiGenerator) Generate() error {
 		"cmd/api/main.go":     getMain(),
 		".gitignore":          getGitignore(),
 		".env":                "# Add environment variables here",
+		"Dockerfile":          generateDockerfile(),
+		"docker-compose.yml":  generateDockerCompose(),
+		".dockerignore":       "config/config.yaml",
 	}
 
 	if err := genDirectories(directories); err != nil {
@@ -37,7 +41,48 @@ func (ag ApiGenerator) Generate() error {
 		return err
 	}
 
-	return nil
+	if err := generateModule(); err != nil {
+		return err
+	}
+
+	return tidy()
+}
+
+func generateModule() error {
+	_, err := os.Open("go.mod")
+
+	if !os.IsNotExist(err) {
+		return nil
+	}
+
+	cmd := exec.Command("go", "mod", "init", getModulePath())
+
+	return cmd.Run()
+}
+
+func getModulePath() string {
+	workDir, err := os.Getwd()
+	if err != nil {
+		return ""
+	}
+
+	return githubIndex(workDir)
+}
+
+func githubIndex(path string) string {
+	index := strings.Index(path, "github.com")
+	if index == -1 {
+		fmt.Println("String 'github.com' not found in path")
+		return ""
+	}
+
+	return path[index:]
+}
+
+func tidy() error {
+	cmd := exec.Command("go", "mod", "tidy")
+
+	return cmd.Run()
 }
 
 func genDirectories(dirs []string) error {
@@ -106,7 +151,27 @@ func getReadme() string {
 }
 
 func getMain() string {
-	content := "package main\n\nfunc main() {\n\n}"
+	configPath := getModulePath() + "/configs"
+	content := fmt.Sprintf(`package main
+
+import (
+	"log"
+	"flag"
+
+	"%s"
+)
+
+func main() {
+	var configPath string
+	flag.StringVar(&configPath, "config-path", "./configs/config.yaml", "Path to config file")
+	flag.Parse()
+
+	config, err := configs.ReadConfig(configPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(config.ServiceName)
+}`, configPath)
 
 	return content
 }
@@ -124,7 +189,7 @@ func getGitignore() string {
 func getConfigyaml() string {
 	content := fmt.Sprintf(`# Put your configuration files here
 
-ServiceName: %s`, strings.ToUpper(getProjectName()))
+serviceName: %s`, strings.ToUpper(getProjectName()))
 
 	return content
 }
@@ -132,33 +197,90 @@ ServiceName: %s`, strings.ToUpper(getProjectName()))
 func getConfig() string {
 	content := `package configs
 
-	import (
-			"os"
-	
-			"gopkg.in/yaml.v3"
-	)
-	
-	type Config struct {
-			ServiceName string 
+import (
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	ServiceName string ` + "`yaml:\"serviceName\"`" + `
+}
+
+func ReadConfig(path string) (*Config, error) {
+	configFile, err := os.ReadFile(path)
+	if err != nil {
+			return nil, err
 	}
 
-	func ReadConfig(path string) (*Config, error) {
-			configFile, err := os.ReadFile(path)
-			if err != nil {
-					return nil, err
-			}
-	
-			return readConfigFromFile(configFile)
-	}
-	
-	func readConfigFromFile(fileBytes []byte) (*Config, error) {
-			cfg := new(Config)
-			if err := yaml.Unmarshal(fileBytes, cfg); err != nil {
-					return nil, err
-			}
-	
-			return cfg, nil
-	}`
+	return readConfigFromFile(configFile)
+}
 
+func readConfigFromFile(fileBytes []byte) (*Config, error) {
+	cfg := new(Config)
+	if err := yaml.Unmarshal(fileBytes, cfg); err != nil {
+			return nil, err
+	}
+
+	return cfg, nil
+}`
+
+	return content
+}
+
+func generateDockerCompose() string {
+	content := fmt.Sprintf(`version: "3.9"
+
+services:
+    %s:
+        container_name: api
+        platform: linux/amd64
+        build:
+            context: .
+            dockerfile: Dockerfile
+        restart: unless-stopped
+        volumes:
+            - ./configs/config.yaml:/app/configs/config.yaml
+        ports:
+            - "8080:8080"
+`, getProjectName())
+	return content
+}
+
+func generateDockerfile() string {
+	content := `# Use specific versions for base images
+FROM golang:1.22.0-alpine3.19 AS builder
+
+WORKDIR /app
+
+# Copy only necessary files for module downloading
+COPY go.mod go.sum ./
+
+# Download dependencies
+RUN go mod download
+
+# Copy the rest of the application code
+COPY . .
+
+# Build the application with optimized flags
+RUN CGO_ENABLED=0 \
+    GOOS=linux \
+    GOARCH=amd64 \
+    go build -ldflags="-s -w" -o /app/main cmd/api/main.go
+
+# Use a smaller base image for the final stage
+FROM alpine:3.19
+
+WORKDIR /app
+
+# Copy built binary and configuration file
+COPY --from=builder /app/main .
+
+# Expose port
+EXPOSE 8080
+
+# Set the entry point with necessary parameters
+ENTRYPOINT ["./main"]
+`
 	return content
 }
